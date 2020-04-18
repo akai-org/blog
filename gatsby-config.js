15adc3043635a2131e09b4c29b77e36e55a906a1
Dawid Dziedzic <dziedzicdev@gmail.com>
2020-04-18 10:12:08 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const postCssPlugins = require('./postcss-config.js');
 module.exports = {
   siteMetadata: {
     url: siteConfig.url,
-    site_url: siteConfig.url,
+    siteUrl: siteConfig.url,
     title: siteConfig.title,
     subtitle: siteConfig.subtitle,
     copyright: siteConfig.copyright,
