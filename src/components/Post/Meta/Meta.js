7d5547a37de04752bca8454eb0a95bd51919af22
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-04-12 18:55:45 +0200

    Translation from English to Polish

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import moment from 'moment';
+import 'moment/locale/pl';
 import styles from './Meta.module.scss';
 
 const Meta = ({ date }) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
+    <p className={styles['meta__date']}>Opublikowano {moment(date).locale('pl').format('D MMMM YYYY')}</p>
   </div>
 );
 
